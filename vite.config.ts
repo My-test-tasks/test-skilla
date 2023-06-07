32f146b06160041e4fd0316a10e53464a12948af
Bumble <bumble.sakh@gmail.com>
2023-06-07 13:49:35 +1100

    feat: add default styles

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     alias: [
       { find: '@components', replacement: path.resolve(__dirname, './src/components') },
       { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+      { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
     ],
   },
 });
