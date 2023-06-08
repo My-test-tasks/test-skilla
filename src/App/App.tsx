5330d29c07707b190af547c19d07f079f8f1c2b9
Bumble <bumble.sakh@gmail.com>
2023-06-08 19:35:41 +1100

    feat: add calls page

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,12 @@
 import Menu from '@components/Menu';
+import Calls from '@pages/Calls';
+import styles from './App.module.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className={styles.app}>
       <Menu />
+      <Calls />
     </div>
   );
 };
---
+++ src/pages/Calls/Calls.tsx
@@ -0,0 +1,7 @@
+import Header from './Header';
+
+const Calls = () => {
+  return <Header />;
+};
+
+export default Calls;
