2349052b4c6279ee399d0aa6aed5b76c970857b7
Bumble <bumble.sakh@gmail.com>
2023-06-09 23:20:33 +1100

    feat: add CellSource styles

--- src/pages/Calls/Table/TableCell/CellSource/CellSource.tsx
+++ src/pages/Calls/Table/TableCell/CellSource/CellSource.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import { ICall } from '@store/calls/types';
+import styles from './CellSource.module.scss';
 
 type CellSourceProps = Pick<ICall, 'source'>;
 
 const CellSource: FC<CellSourceProps> = ({ source }) => {
-  return <td>{source}</td>;
+  return <td className={styles.cell}>{source}</td>;
 };
 
 export default CellSource;
