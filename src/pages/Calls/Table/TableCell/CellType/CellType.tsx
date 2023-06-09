c5b848721d2ab8aaa2759028ab7e582e23a1fd0f
Bumble <bumble.sakh@gmail.com>
2023-06-09 22:39:33 +1100

    feat: add CellType component

---
+++ src/pages/Calls/Table/TableCell/CellType/CellType.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import call_01 from '@assets/icons/call_01.svg';
+import call_02 from '@assets/icons/call_02.svg';
+import call_03 from '@assets/icons/call_03.svg';
+import { ICall } from '@store/calls/types';
+
+type CellTypeProps = Pick<ICall, 'in_out'>;
+
+const CellType: FC<CellTypeProps> = ({ in_out: type }) => {
+  let icon;
+
+  switch (type) {
+    case 0:
+      icon = call_03;
+      break;
+
+    case 1:
+      icon = call_01;
+      break;
+
+    default:
+      icon = call_02;
+      break;
+  }
+
+  return (
+    <td>
+      <img src={icon} alt='icon' />
+    </td>
+  );
+};
+
+export default CellType;
