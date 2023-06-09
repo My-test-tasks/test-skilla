158386f4e2d601c21d3bcb40874307873c8e303a
Bumble <bumble.sakh@gmail.com>
2023-06-09 22:46:02 +1100

    feat: add fetching calls on page

--- src/pages/Calls/Calls.tsx
+++ src/pages/Calls/Calls.tsx
@@ -1,14 +1,30 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import BallanceBar from './BallanceBar';
 import FilterBar from './FilterBar';
 import styles from './Calls.module.scss';
+import { useAppDispatch, useAppSelector } from '@store/hooks';
+import { fetchCallsRequest } from '@store/calls/actions';
+import Table from './Table';
 
 const Calls = () => {
+  const { pending, error } = useAppSelector((state) => state.calls);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCallsRequest());
+  }, [dispatch]);
+
   return (
     <div className={styles.calls}>
       <Header />
       <BallanceBar />
       <FilterBar />
+
+      {pending && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      {!pending && !error && <Table />}
     </div>
   );
 };
