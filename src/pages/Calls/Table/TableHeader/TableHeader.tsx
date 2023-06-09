ad3746045a60e27aba93fcda69648d2db5bb4a03
Bumble <bumble.sakh@gmail.com>
2023-06-09 22:44:32 +1100

    feat: add TableHeader

---
+++ src/pages/Calls/Table/TableHeader/TableHeader.tsx
@@ -0,0 +1,18 @@
+const TableHeader = () => {
+  return (
+    <thead>
+      <tr>
+        <th></th>
+        <th>Тип</th>
+        <th>Время</th>
+        <th>Сотрудник</th>
+        <th>Звонок</th>
+        <th>Источник</th>
+        <th>Оценка</th>
+        <th>Длительность</th>
+      </tr>
+    </thead>
+  );
+};
+
+export default TableHeader;
