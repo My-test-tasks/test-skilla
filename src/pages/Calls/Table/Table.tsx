6e0c8934c7dfb9c9d6167ffb32aad41aa567b30d
Bumble <bumble.sakh@gmail.com>
2023-06-21 23:18:20 +1100

    refactor: table cols width

--- src/pages/Calls/Table/Table.tsx
+++ src/pages/Calls/Table/Table.tsx
@@ -9,6 +9,17 @@ const Table = () => {
 
   return (
     <table className={styles.table}>
+      <colgroup>
+        <col className={styles['column-1']} />
+        <col className={styles['column-2']} />
+        <col className={styles['column-3']} />
+        <col className={styles['column-4']} />
+        <col className={styles['column-5']} />
+        <col className={styles['column-6']} />
+        <col className={styles['column-7']} />
+        <col className={styles['column-8']} />
+      </colgroup>
+
       <TableHeader />
 
       <tbody>
